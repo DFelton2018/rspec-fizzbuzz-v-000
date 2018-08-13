d43f831ca478a617fb71605c581bba509f4d2b30
Duane Felton <dfelton2010@gmail.com>
2018-08-13 15:27:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,18 @@
 def fizzbuzz(int)
+<<<<<<< HEAD
   if int % 3 == 0&&int % 5 == 0
     return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
   elsif int % 3 == 0
     return "Fizz"
+=======
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 3&&5 == 0
+    return "FizzBuzz"
+>>>>>>> 270663d6821b73c3360537fc3c86abd530f372fa
 end
 end
